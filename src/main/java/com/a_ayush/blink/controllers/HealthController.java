27f50bffabd564046057eae70f62b4ba21877903
vAayush3301 <vaayush3301@gmail.com>
2026-05-15 00:42:15 +0530

    Blink

---
+++ src/main/java/com/a_ayush/blink/controllers/HealthController.java
@@ -0,0 +1,13 @@
+package com.a_ayush.blink.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Blinking...";
+    }
+}
